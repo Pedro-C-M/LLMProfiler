e325a088f3c6dccd944c072e793b38e3bae4a2e2
Pedro-C-M <pedrocastromontes@gmail.com>
2026-05-25 11:28:52 +0200

    Separación en logger de consola + logger de archivo .txt sin librería Rich

--- logger/log.py
+++ logger/log.py
@@ -2,8 +2,22 @@ import logging
 import logging.handlers
 from rich.logging import RichHandler
 import os
+import re
 
 EXECUTION_PATH = os.path.dirname(os.path.realpath(__file__))
+
+def clean_rich_markup(text):
+    return re.sub(r'\[\/?[a-zA-Z0-9\s#_,]+\]', '', text)
+
+class PlainFileFormatter(logging.Formatter):
+    def format(self, record):
+        original_msg = record.msg
+        if isinstance(record.msg, str):
+            record.msg = clean_rich_markup(record.msg).replace('\\', '')
+        
+        result = super().format(record)
+        record.msg = original_msg
+        return result
 
 class Log(logging.Logger):
     
@@ -18,11 +32,17 @@ class Log(logging.Logger):
     def debug_color(self, msg: str, *args, **kwargs): 
             super().debug(f"[i bold green]{msg}", extra= {"markup" : True}, stacklevel=2)
 
-
-file_handler = logging.FileHandler(f"{EXECUTION_PATH}/logs.txt", mode = "w", encoding= "utf-8")
-FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-logging.basicConfig(
-    level = "NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(), file_handler]
-)
 logging.setLoggerClass(Log)
+
+FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+file_handler = logging.FileHandler(f"{EXECUTION_PATH}/logs.txt", mode = "w", encoding= "utf-8")
+file_formatter = PlainFileFormatter(FORMAT, datefmt="[%X]")
+file_handler.setFormatter(file_formatter)
+console_handler = RichHandler(markup=True, show_path=False)
+
+
+logging.basicConfig(
+    level = "NOTSET", format=FORMAT, datefmt="[%X]", handlers=[console_handler, file_handler]
+)
+
 logger = logging.getLogger("llm-eval")
